89d26b0f3362cf54a2a0f5e618c9275421403cb4
Surya Letha S <surya.sudarsanan111@gmail.com>
2025-06-06 20:53:44 +0530

    Add comprehensive SQL query set for sales data analysis
    This commit includes a complete SQL script demonstrating key concepts of structured data manipulation and analysis using SQL Server. The script covers:
    
    - Basic SELECT queries with WHERE and ORDER BY clauses
    - GROUP BY with aggregate functions like SUM and AVG
    - Subqueries to filter customer-level insights
    - JOINs (INNER, LEFT, RIGHT) using a dummy customer_info table
    - View creation for reusable summary insights
    - Index creation to optimize performance on frequent filter columns
    
    These queries are based on a sample sales dataset and were developed as part of an internship learning exercise. The file is designed for educational and practice purposes using SQL Server Management Studio (SSMS).

---
+++ ecommerce_data_sql.sql
@@ -0,0 +1,68 @@
+USE ecommercedb;
+
+CREATE TABLE sales_data (
+    InvoiceNo VARCHAR(20),
+    StockCode VARCHAR(20),
+    Description VARCHAR(255),
+    Quantity INT,
+    InvoiceDate DATETIME,
+    UnitPrice FLOAT,
+    CustomerID INT,
+    Country VARCHAR(50)
+);
+
+BULK INSERT sales_data
+FROM 'C:\Users\Arya\Desktop\ElevateLab\Task4\ecommerce_data.csv'
+WITH (
+    FORMAT = 'CSV',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TABLOCK
+);
+
+	SELECT * FROM sales_data;
+
+SELECT * FROM sales_data
+WHERE Country = 'France';
+
+-- most expensive items
+SELECT Description, UnitPrice FROM sales_data
+ORDER BY UnitPrice DESC;
+
+-- GROUP BY with aggregate: Total quantity sold by country
+SELECT Country, SUM(Quantity) AS TotalQuantity
+FROM sales_data
+GROUP BY Country
+ORDER BY TotalQuantity DESC;
+
+-- JOIN
+-- Creating a dummy product category table
+CREATE TABLE product_categories (
+    StockCode VARCHAR(20),
+    Category VARCHAR(50)
+);
+
+-- Simulate JOIN: sales with product category
+SELECT s.InvoiceNo, s.Description, p.Category
+FROM sales_data s
+LEFT JOIN product_categories p ON s.StockCode = p.StockCode;
+
+--Customers who bought more than 100 items
+SELECT DISTINCT CustomerID
+FROM sales_data
+WHERE CustomerID IN (
+    SELECT CustomerID
+    FROM sales_data
+    GROUP BY CustomerID
+    HAVING SUM(Quantity) > 100
+);
+
+--Average unit price per country
+SELECT Country, AVG(UnitPrice) AS AvgPrice
+FROM sales_data
+GROUP BY Country;
+
+-- Indexing for optimization
+CREATE INDEX idx_customer ON sales_data(CustomerID);
+CREATE INDEX idx_country ON sales_data(Country);
